587a218bce9c09f8bd2f88af4a5fc0a33f6f7658
Edris Berg <ali_berg1@yahoo.com>
2021-11-03 18:51:16 -0700

    lab07 done

--- server.js
+++ server.js
@@ -6,15 +6,49 @@ const cors = require('cors');
 const weather = require('./weather.json');
 
 const app = express();
-
 app.use(cors());
+const PORT = process.env.PORT || 3001;
 
-const PORT = process.env.PORT;
-
-app.get('/hello', (request, response) => { response.send('Hey, there!') });
 app.get('/weather', handleGetWeather);
+app.get('/*', (re, res) => res.status(404).send('not found'));
+
 
 function handleGetWeather(req, res) {
-    res.status(200).send(weather);
+
+    const cityName = req.query.city;
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    try {
+        console.log(cityName);
+        const cityToSend = weather.find(city => {
+
+            if ((city.lat === lat && city.lon === lon) || city.city_name === cityName) {
+
+                return true
+            }
+            return false;
+        });
+
+        if (cityToSend) {
+            const forecastData = cityToSend.data.map(city => new weatherForecast(city));
+            res.status(200).send(forecastData);
+
+        } else {
+            res.status(404).send('Not found');
+        }
+
+    } catch (e) {
+        res.status(500).send('server error');
+    }
 }
-app.listen(3001, () => console.log(`hey, I am a server on port: ${PORT}`));
+
+class weatherForecast {
+    constructor(obj) {
+        this.min_temp = obj.min_temp;
+        this.max_temp = obj.max_temp;
+        this.description = obj.weather.description;
+
+    }
+}
+
+app.listen(PORT, () => console.log(`hey, I am running on port: ${PORT}`));
