6c74b91e3d928ddd43664b1efc67729979e2cf01
Edris Berg <ali_berg1@yahoo.com>
2021-11-07 13:58:46 -0800

    done with server-class-10

--- movies.js
+++ movies.js
@@ -1,19 +1,34 @@
 const axios = require('axios');
+let cache = require('./cache.js');
 
 async function handleGetMovies(req, res) {
     const { city } = req.query;
-    //console.log("this is the city" + city)
+    console.log("this is the city" + city)
 
     const url = `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.MOVIE_DB_API_KEY}&query=${city}&language=en-US&include_adult=false`;
 
-    try {
-        const result = await axios.get(url);
-        const MoviesData = result.data.results.map(city => new Movie(city));
-        //console.log(MoviesData)
-        res.status(200).send(MoviesData);
+    const key = 'movie-' + city;
 
+    if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+        console.log('Cache hit');
+    } else {
+        console.log('Cache miss');
+        cache[key] = {};
+        cache[key].timestamp = Date.now();
+        cache[key].data = axios.get(url);
+            .then(response => parseMovies(response.data));
+    }
+    return cache[key].data;
+}
+
+function parseMovies(movieData) {
+    try {
+        const movieSummaries = movieData.results.map(day => {
+            return new Movie(day);
+        });
+        return Promise.resolve(movieSummaries);
     } catch (e) {
-        res.status(500).send('server error');
+        return Promise.reject(e);
     }
 }
 
